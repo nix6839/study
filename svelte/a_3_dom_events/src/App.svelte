a945eea4194288b0c0dea6bd1d026a17b860fd30
Nix <nix6839@washnix.com>
2022-06-27 08:33:49 +0900

    Add a_3_dom_events

---
+++ svelte/a_3_dom_events/src/App.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  const m = { x: 0, y: 0 };
+
+  function onMouseMove(event: MouseEvent) {
+    m.x = event.clientX;
+    m.y = event.clientY;
+  }
+</script>
+
+<div on:mousemove={onMouseMove}>
+  The mouse position is {m.x} x {m.y}
+</div>
+
+<style>
+  :global(body) {
+    margin: 0;
+  }
+  :global(#app) {
+    height: 100vh;
+  }
+  div {
+    width: 100%;
+    height: 100%;
+  }
+</style>

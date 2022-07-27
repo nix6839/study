24e2451e4874045909c4eb8226b9241bbeda585a
Han Yeong-woo <nix6839@washnix.com>
2022-07-27 22:54:56 +0900

    Add a_1_react-test-app

---
+++ react/a_1_react-test-app/src/App.tsx
@@ -0,0 +1,25 @@
+import './App.css';
+import reactLogo from './assets/react.svg';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={reactLogo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ react/a_1_react-test-app/vite.config.ts
@@ -0,0 +1,19 @@
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 3000,
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './__tests__/setup.ts',
+    css: true,
+  },
+});

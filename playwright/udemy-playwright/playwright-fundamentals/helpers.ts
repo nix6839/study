b336650258127ec3b0fb367c421bb11449f8a3ca
Han Yeong-woo <nix6839@washnix.com>
2022-09-18 12:03:40 +0900

    compl: playwright-fundamentals

---
+++ playwright/udemy-playwright/playwright-fundamentals/helpers.ts
@@ -0,0 +1,9 @@
+import { Page } from '@playwright/test';
+
+export async function loadHomepage(page: Page) {
+  page.goto('https://www.example.com');
+}
+
+export async function assertTitle(page: Page) {
+  await page.waitForSelector('h1');
+}
